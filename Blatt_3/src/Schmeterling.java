b29543386982222db8f488301893a40810c80a55
Robi <stertz.robert@gmx.de>
2021-04-01 01:11:04 +0200

    Anfang Aufgabe 9

---
+++ Blatt_3/src/Schmeterling.java
@@ -0,0 +1,6 @@
+public class Schmeterling extends Tier {
+    
+    Schmeterling(int posx, int posy, double gewicht, int flug, int luft, int aqua){
+
+    }
+}
---
+++ Blatt_3/src/Tier.java
@@ -0,0 +1,29 @@
+import static jsTools.Graph.*;
+
+public class Tier {
+
+    int posx;
+    int posy;
+    double fluegelspannweite;
+    double gewicht;
+    int temp;
+    int keafig;
+    int aqurium;
+
+    void paint(int b, int h, String file) {
+        addPicture(posx, posy, b, h, file);
+    }
+
+    void fortbewegung(){
+
+    }
+
+    void versteandigung(){
+
+    }
+
+    void liebligsbeutetier() {
+
+    }
+
+}
